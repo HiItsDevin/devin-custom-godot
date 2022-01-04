6d680e0563d46a53b62eeb57b6f0ca104ddfe248
Devin <devin@hiitsdevin.dev>
2022-01-04 04:38:33 -0800

    trying custom.py and failing.

---
+++ custom.py
@@ -0,0 +1,12 @@
+# custom.py
+
+OBJPREFIX = "devins-custom-"
+PROGPREFIX = "devins-custom-"
+LIBPREFIX = "devins-custom-"
+SHLIBPREFIX = "devins-custom-"
+optimize = "size"
+module_mono_enabled = "no"
+use_llvm = "yes"
+t = "release_debug"
+tools = "yes"
+dev = "true"
